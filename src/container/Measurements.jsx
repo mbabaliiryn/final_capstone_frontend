c8b40921a47603b691312e58b938d64d83c79af0
mbabali iryn <mbabaliiryn@gmail.com>
2020-10-13 11:50:06 +0300

    add user authenticatio reducers

---
+++ src/container/Measurements.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect } from 'react'
+import Measurement from '../components/Measurement'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchMeasurements } from '../actions/measurementsAction'
+import { Link } from 'react-router-dom'
+import Footer from '../components/Footer'
+
+const Measurements = (props) => {
+    const activityId = props.location.state
+    const actTitle = props.location.actTitle
+    const dispatch = useDispatch()
+    let timeDifference = []
+    let diffVal = []
+
+    useEffect(()=> {
+        dispatch(fetchMeasurements(activityId))
+    }, [activityId, dispatch])
+
+    const measurements = useSelector(state => state.measurementsReducer.measurements)
+  
+    for(let i = 0; i < measurements.length; i++){
+        timeDifference.push(parseFloat(measurements[i].duration))
+        if(timeDifference.length === 1){
+             diffVal.push(timeDifference[0])
+        }else if ( timeDifference.length > 1){
+            diffVal.push(
+                timeDifference[timeDifference.length - 2] - timeDifference[timeDifference.length - 1]
+                )
+        }
+    }
+
+    return (
+        <div>
+            <div>
+                <button> 
+                    <Link to={{
+                        pathname: `/activity/${activityId}/create`,
+                        state : activityId,
+                        actTitle : actTitle
+                    }}>
+                        Add
+                    </Link>
+                 </button>
+            </div>
+            <div>
+                {
+                    measurements.map((measurement, index) => (
+                        <Measurement 
+                            key = { index }
+                            index = { index } 
+                            measurement = { measurement } 
+                            diffVal = { diffVal }/>
+                    ))
+                }
+            </div>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Measurements
