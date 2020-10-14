d3df28dc4c653e4fd7f91dd6aca9bc54df585e45
mbabali iryn <mbabaliiryn@gmail.com>
2020-10-14 10:51:58 +0300

    add routers

---
+++ src/components/Router.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Signin from './auth/Signin'
+import Activities from '../container/Activities'
+import Navbar from './Navbar'
+import CreateActivity from './CreateActivity'
+import Measurements from '../container/Measurements'
+import CreateMeasurement from '../components/CreateMeasurement'
+import PrivateRoute from './PrivateRoute'
+import Signup from '../components/auth/Signup'
+
+const Router = () => {
+    return (
+        <div>
+           <Navbar/> 
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/signin" component= { Signin }/>
+                    <Route exact path="/signup" component= { Signup }/>
+                    <PrivateRoute exact path="/activities" component= { Activities }/>
+                    <PrivateRoute exact path="/create-activity" component={CreateActivity}/>
+                    <PrivateRoute exact path="/activity/:id/measurements" component={ Measurements }/>
+                    <PrivateRoute exact path="/activity/:id/create" component={ CreateMeasurement }/>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    )
+}
+
+export default Router
